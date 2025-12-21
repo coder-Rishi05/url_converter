5c8700e65eb64272e505eca049596f0ed6ce59c1
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-12-22 00:25:17 +0530

    cheking url validation

--- routes/check.js
+++ routes/check.js
@@ -2,12 +2,32 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/data", async (req, res) => {
+router.post("/check", async (req, res) => {
   try {
-    const url = await req.body;
-    const jsonUrl = await url.json();
+    const { url } = req.body;
+
+    // validation for checking url present or not
+    // validation for checking url present or not
+    if (!url) {
+      return res
+        .status(400)
+        .json({ success: false, message: "URL is required" });
+    }
+
+    // validation for checking url start with https & https
+
+    if (!url.startWith("https://") && !url.startWith("http://")) {
+      return res.status(400).json({
+        success: false,
+        message: "not valid url format",
+      });
+    }
   } catch (error) {
     console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
   }
 });
 
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
-import router from "./routes/check";
+import checkRoute from "./routes/check.js";
+
 const app = express();
 
-app.use(router);
+app.use(express.json());
+
+app.use("/api", checkRoute);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my site");
