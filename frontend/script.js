1a0ff58441b7ca6027c11b53e87288cdd1ddeb96
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-12-27 11:06:21 +0530

    fixed cors error

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,49 @@
-async function check() {
-  const url = document.getElementById("url").value;
+// async function check() {
+//   const url = document.getElementById("url").value;
 
-  const res = await fetch("http://localhost:5000/check", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ url }),
-  });
+//   const res = await fetch("http://localhost:3000/api/check", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ url }),
+//   });
 
-  const data = await res.json();
-  document.getElementById("out").innerText = JSON.stringify(data, null, 2);
-}
+//   const data = await res.json();
+//   document.getElementById("out").innerText = JSON.stringify(data, null, 2);
+// }
+
+// async function download() {
+//   const url = document.getElementById("url").value;
+
+//   if (!url) {
+//     alert("Please enter a URL");
+//     return;
+//   }
+
+//   const res = await fetch("http://localhost:3000/api/download", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       url: url,
+//       type: "image", // abhi version 1 me fixed
+//     }),
+//   });
+
+//   if (!res.ok) {
+//     const err = await res.json();
+//     alert(err.message || "Download failed");
+//     return;
+//   }
+
+//   // 👇 Browser download logic
+//   const blob = await res.blob();
+//   const downloadUrl = window.URL.createObjectURL(blob);
+
+//   const a = document.createElement("a");
+//   a.href = downloadUrl;
+//   a.download = "downloaded-file.jpg"; // naam frontend se
+//   document.body.appendChild(a);
+//   a.click();
+
+//   a.remove();
+//   window.URL.revokeObjectURL(downloadUrl);
+// }
--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-  import express from "express";
-  import checkRoute from "./routes/check.js";import downloadRoute from "./routes/download.js";
+import express from "express";
+import checkRoute from "./routes/check.js";
+import downloadRoute from "./routes/download.js";
+import cors from "cors"
+const app = express();
 
-  
-  
-  const app = express();
-  
-  app.use(express.json());
-  
-  
-  app.get("/", (req, res) => {
-    res.send("Welcome to my site");
-  });
-  app.use("/api", checkRoute);
-  app.use("/api", downloadRoute);
+app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to my site");
+});
+app.use("/api", checkRoute);
+app.use("/api", downloadRoute);
 
-  app.listen(3000, () => {
-    console.log(`Server running at  : 3000`);
-  });
+app.listen(3000, () => {
+  console.log(`Server running at  : 3000`);
+});
